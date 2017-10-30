ef920e69872e6f99ed9f22027cab9764f05539bb
Sebastian Kürten <sebastian@topobyte.de>
2017-10-30 15:29:26 +0100

    Add an example class

---
+++ src/main/java/de/topobyte/MyUtil.java
@@ -0,0 +1,16 @@
+package de.topobyte;
+
+public class MyUtil
+{
+
+	public static void printSomething()
+	{
+		System.out.println("Hello JSweet");
+	}
+
+	public static String returnSomething()
+	{
+		return "Hello JSweet";
+	}
+
+}
